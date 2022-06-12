51660484d41aaaa62c1ea1dee8eb7c8f7fff4ba4
Vasanth Developer <vsnthdev@gmail.com>
2022-06-12 17:42:42 +0530

    ⚡ implemented custom scraping logic

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@
  */
 
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { youtube } from 'scrape-youtube'
+import axios from 'axios'
 
 // https://snaplink.dev/4d87b6bd7c?video=NhEn4H5UK9A&color=208ef3&w=1200&h=630
 
@@ -18,27 +18,35 @@ export default async (
     if (video == 'favicon.ico') return res.status(204).send('')
     
     // fetch video information
-    const data = (await youtube.search(video as string)).videos[0]
+    const { data: vid }: { data: string } = await axios({
+        method: 'GET',
+        url: `https://www.youtube.com/watch?v=${video}`
+    })
+
+    const meta = vid.match(/<meta[^>]+content="([^")]*)"/g) as string[]
+
+    const title = meta.find(tag => tag.startsWith('<meta name="title" '))?.split('"')[3]
+    const desc = meta.find(tag => tag.startsWith('<meta name="description" '))?.split('"')[3]
 
     return res.send(`
     <!DOCTYPE html>
     <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <title>${data.title} - YouTube</title>
+            <title>${title} - YouTube</title>
             <meta http-equiv="refresh" content="0;url=https://www.youtube.com/watch?v=${video}" />
 
-            <meta name="description" content="${data.description}" />
+            <meta name="description" content="${desc}" />
             <meta name="referrer" content="no-referrer-when-downgrade" />
-            <meta property="og:site_name" content="${data.title} - YouTube" />
+            <meta property="og:site_name" content="${title} - YouTube" />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content="${data.title} - YouTube" />
-            <meta property="og:description" content="${data.description}" />
+            <meta property="og:title" content="${title} - YouTube" />
+            <meta property="og:description" content="${desc}" />
             <meta property="og:url" content="https://www.youtube.com/watch?v=${video}" />
             <meta property="og:image" content="https://img.snaplink.dev/4d87b6bd7c/?video=${video}&color=208ef3&w=1200&h=630" />
 
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="${data.title} - YouTube" />
+            <meta name="twitter:title" content="${title} - YouTube" />
             <meta name="twitter:url" content="https://www.youtube.com/watch?v=${video}" />
             <meta name="twitter:image" content="https://img.snaplink.dev/4d87b6bd7c/?video=${video}&color=208ef3&w=1200&h=630" />
             <meta property="og:image:width" content="1200" />
